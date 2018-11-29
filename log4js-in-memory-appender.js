25dc51df2cf7cfa057d792d317a1ea98981463ff
Ni-vek <kevin.balini@gmail.com>
2018-11-29 17:00:56 +0100

    Add some comments

--- log4js-in-memory-appender.js
+++ log4js-in-memory-appender.js
@@ -1,4 +1,4 @@
-// Workaround du to log4js external appenders loading mechanism
+// Workaround due to log4js external appenders loading mechanism
 const log4jsInMemoryAppender = require('./src');
 
 module.exports = log4jsInMemoryAppender;
